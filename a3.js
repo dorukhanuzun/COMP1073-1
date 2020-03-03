0a9ce3135a089e74d25424b5eb2bddfa91632977
Dorukhan Uzun <200427534@student.georgianc.on.ca>
2020-03-03 13:48:43 -0500

    Assignment 3 Part 1

---
+++ a3.js
@@ -0,0 +1,62 @@
+const menu = [
+    {
+        title: 'Menu 1',
+        soup: 'Lentil',
+        soupPrice: '2.00',
+        salad: 'Turkish',
+        saladPrice: '5.00',
+        lighterFare: 'Ham&Chicken Panini',
+        lighterFarePrice: '5.00',
+        entree: 'Breaded Chicken on a Bun',
+        entreePrice: '2.95',
+        availableOnWhichDay: 'Thursday'
+    },
+    {
+        title: 'Menu 2',
+        soup: 'Clam Chowder',
+        soupPrice: '2.60',
+        salad: 'Israeli Couscous&Feta Salad',
+        saladPrice: '2.40',
+        lighterFare: 'Pulled Pork on Bun',
+        lighterFarePrice: '4.95',
+        entree: 'Matar Paneer & Basmati Rice',
+        entreePrice: '5.95',
+        availableOnWhichDay: 'Wednesday'
+    },
+    {
+        title: 'Menu 3',
+        soup: 'Tomato',
+        soupPrice: '2.20',
+        salad: 'Caesar Salad',
+        saladPrice: '2.90',
+        lighterFare: 'Turkey Panini',
+        lighterFarePrice: '6.00',
+        entree: 'Beef&Broccoli with Fried Rice',
+        entreePrice: '7.60',
+        availableOnWhichDay: 'Tuesday'
+    },
+    {
+        title: 'Menu 4',
+        soup: 'Spicy Squash & Pumpkin',
+        soupPrice: '4.00',
+        salad: 'Mediterranean Salad',
+        saladPrice: '6.50',
+        lighterFare: 'Madras Chicken Salad Wrap',
+        lighterFarePrice: '7.00',
+        entree: 'Fish & Chips',
+        entreePrice: '6.00',
+        availableOnWhichDay: 'Monday'
+    },
+    {
+        title: 'Menu 5',
+        soup: 'Chicken',
+        soupPrice: '3.00',
+        salad: 'Greek',
+        saladPrice: '4.00',
+        lighterFare: 'Burger & Fries',
+        lighterFarePrice: '6.00',
+        entree: 'Breaded Turkey on a Bun',
+        entreePrice: '4.95',
+        availableOnWhichDay: 'Friday'
+    }
+]
